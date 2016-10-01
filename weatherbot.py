376c8049219589f9e8907597eebe0694ecc431f9
Alan Cunningham <ajc.alan@gmail.com>
2016-10-02 00:08:28 +0100

    Added the ability to subscribe/unsub from scheduled weather updates

--- weatherbot.py
+++ weatherbot.py
@@ -20,26 +20,50 @@ updater = Updater(token=token)
 # Setup logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
 is_running = True
-message_groups = []
+message_groups = {}
 weather_timeout = 3600
 response_timeout = 20
 weather_summary = None
-
-# Run start() when bot receives the /start command
+scheduler_running = False
 dispatcher = updater.dispatcher
-def start(bot, update):
-	if update.message.chat_id not in message_groups:
-		send_message(bot, update, '**INITIALISING**')
+
+
+# Respond to certain keywords in the chat
+# There's most likely a better way of doing this...
+def custom_responses(bot, update):
+	global response_timeout
+	# Start the scheduler on startup
+	start_scheduler(bot, update)
+	message = update.message.text.lower()
+	if 'hi sam' == message:
+		send_message(bot, update, '**HELLO THERE**')
+	if 'sam' == message:
+		send_message(bot, update, '**WHAT**')
+	if get_timeout_diff(response_timeout) > 900:
+		if 'red lion' in message:
+			send_message(bot, update, 'Which one?')
+		response_timeout = int(time.time())
+
+custom_responses = MessageHandler([Filters.text], custom_responses)
+dispatcher.add_handler(custom_responses)
+
+
+def start_scheduler(bot, update):
+	global scheduler_running, message_groups
+	if scheduler_running == False:
+		logging.info('Starting schedule')
 		# Schedule a weather report at a certain time
-		schedule.every().day.at('08:00').do(schedule_weather, message_groups, bot, update)
+		schedule.every().day.at('08:00').do(send_scheduled_weather, bot, update)
+		scheduler_running = True
 		# Run the scheduler in the background
 		t = threading.Thread(target=run_scheduler)
 		t.daemon = True
 		t.start()
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+	if update.message.chat_id not in message_groups:
+		subscribe_group(bot, update)
 
 
 # Weather scheduler
@@ -50,37 +74,44 @@ def run_scheduler():
 
 
 # Subscribe a group to scheduled weather updates
-def schedule_on(bot, update):
+def subscribe_group(bot, update):
+	global message_groups
 	if update.message.chat_id not in message_groups:
-		message_groups.append(update.message.chat_id)
-		send_message(bot, update, '**SCHEDULE ENABLED**')
+		message_groups[update.message.chat_id] = {
+				'subscribed': True
+		}
+	else:
+		message_groups[update.message.chat_id]['subscribed'] = True
+		send_message(bot, update, '**SUBSCRIBED**')
 	logging.info('Message groups: %s ' % message_groups)
 
 
-schedule_on_handler = CommandHandler('schedule_on', schedule_on)
-dispatcher.add_handler(schedule_on_handler)
+subscribe_group_handler = CommandHandler('subscribe', subscribe_group)
+dispatcher.add_handler(subscribe_group_handler)
 
 
 # Unsubscribe a group from scheduled weather updates
-def schedule_off(bot, update):
-	if update.message.chat_id in message_groups:
-		message_groups.remove(update.message.chat_id)
-		send_message(bot, update, '**SCHEDULE DISABLED**')
+def unsubscribe_group(bot, update):
+	global message_groups
+	message_groups[update.message.chat_id]['subscribed'] = False
+	send_message(bot, update, '**UNSUBSCRIBED**')
 	logging.info('Message groups: %s ' % message_groups)
 
-schedule_off_handler = CommandHandler('schedule_off', schedule_off)
-dispatcher.add_handler(schedule_off_handler)
+unsubscribe_group_handler = CommandHandler('unsubscribe', unsubscribe_group)
+dispatcher.add_handler(unsubscribe_group_handler)
 
 
-def schedule_weather(bot, update):
+def send_scheduled_weather(bot, update):
+	global message_groups
 	for group in message_groups:
-		summary = request_weather()
-		logging.info('Group: %s ' % group)
-		bot.sendMessage(chat_id=group, text=request_weather())
+		if message_groups[group]['subscribed'] == True:
+			summary = get_weather()
+			logging.info('Group: %s ' % group)
+			bot.sendMessage(chat_id=group, text=get_weather())
 
 
 # Return a daily weather report
-def request_weather():
+def get_weather():
 	global weather_timeout, weather_summary
 	# Update the weather if we haven't requested it in a while.
 	# If we've recently requested the weather, we just return the cached version
@@ -99,25 +130,8 @@ def request_weather():
 		weather_timeout = int(time.time())
 	return weather_summary
 
-weather_handler = CommandHandler('weather', lambda x,y: send_message(x, y, request_weather()))
+weather_handler = CommandHandler('weather', lambda x,y: send_message(x, y, get_weather()))
 dispatcher.add_handler(weather_handler)
-
-# Respond to certain keywords in the chat
-# There's most likely a better way of doing this...
-def custom_responses(bot, update):
-	global response_timeout
-	message = update.message.text.lower()
-	if 'hi sam' == message:
-		send_message(bot, update, '**HELLO THERE**')
-	if 'sam' == message:
-		send_message(bot, update, '**WHAT**')
-	if get_timeout_diff(response_timeout) > 900:
-		if 'red lion' in message:
-			send_message(bot, update, 'Which one?')
-		response_timeout = int(time.time())
-
-custom_responses = MessageHandler([Filters.text], custom_responses)
-dispatcher.add_handler(custom_responses)
 
 
 # Return how many seconds have elapsed since now and a given timestamp
