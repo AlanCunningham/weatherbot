83d1a01e0a2434369e3cffbcb1ecb4ec6afffb6a
Alan Cunningham <ajc.alan@gmail.com>
2016-10-23 11:35:51 +0100

    Removed API keys from config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 [darksky]
-api_key: 5e6a19b1d694c99f44d13cfbf4a767d6
+api_key: darksky_api_key_here
 lon: 51.5074
 lat: -0.1278
 units: uk2
 
 [telegram]
-api_key: 261928620:AAFkvFXahgdeTU3wAIHH4V1MywXERboVkkA
+api_key: telegram_api_key_here
